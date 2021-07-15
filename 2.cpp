4a377c299cf3de0bc20bdaea0bd95e52f314d36c
byungju0624 <byungju0624@gmail.com>
2021-07-16 02:48:10 +0900

    if&switch study

--- 2.cpp
+++ 2.cpp
@@ -53,12 +53,83 @@
 //관계 연산자 -> 0 input false, else number input true 
 //            false output 0, true output 1
 
+// int main(void){
+//     int a, b;
+//     scanf("%d %d",&a, &b);
+//     printf("%d\n",a>b);
+//     printf("%d\n",a==b);
+//     printf("%d\n",a!=b);
+//     printf("%d\n",a>=b);
+//     printf("%d\n",a<=b);
+// }
+
+//조건문 1. if 문
+/*
+if(조건1){
+    조건1이 만족할 떄 출력
+}else if(조건2){
+    조건1이 만족하지 않고 조건2가 만족할때
+}else{
+    모든 조건이 만족하지 않을때
+}
+*/
+
+// int main(void){
+//     printf("손님이 몇 명인가요?");
+//     int a;
+//     scanf("%d",&a);
+//     if(a == 1 || a == 2){
+//         printf("2인석으로 안내합니다.\n");
+//     }else if(a == 3 || a == 4){
+//         printf("4인석으로 안내합니다.\n");
+//     }else{
+//         printf("대형석으로 안내합니다.\n");
+//     };
+// }
+
+// int main(void){
+//     int size;
+//     printf("메인 메모리 크기를 입력하세요. (GB)");
+//     scanf("%d", &size);
+//     if(size >= 16){
+//         printf("메인 메모리 크기가  충분합니다.\n");
+//     }else{
+//         printf("메인 메모리를 증설하세요.\n");
+//     }
+// }
+
+
+//swich문
+/*
+swich(확인 대상){
+    case 값1:
+    //값 1에 부합할 떼
+    case 값2:
+    //값2에 부합할 때
+    Default:
+    //모든 경우
+}
+*/
+
 int main(void){
-    int a, b;
-    scanf("%d %d",&a, &b);
-    printf("%d\n",a>b);
-    printf("%d\n",a==b);
-    printf("%d\n",a!=b);
-    printf("%d\n",a>=b);
-    printf("%d\n",a<=b);
+    printf("학점을 입력하세요. ");
+    char a;
+    scanf("%c", &a);
+    switch(a){
+        case 'A':
+        printf("A학점입니다.\n");
+        break;
+        case 'B':
+        printf("B학점입니다.\n");
+         break;
+        case 'C':
+        printf("C학점입니다.\n");
+         break;
+        case 'D':
+        printf("D학점입니다.\n");
+         break;
+        default:
+        printf("학점을 바르게 입력하세요.\n");
+         break;
+    }
 }
